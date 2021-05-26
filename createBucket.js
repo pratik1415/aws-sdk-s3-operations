a4c36279c7a1c3502d161007c2901dbde900a706
pratik-dell <pratikshelar1510@gmail.com>
2021-05-26 18:17:20 +0530

    AWS SDK S3 Operations

---
+++ createBucket.js
@@ -0,0 +1,62 @@
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
+var file = require('./package.json')
+
+AWS.config.update({
+    region: "ap-south-1",
+    accessKeyId: "AKIASIGLOA7NW66O4OTB",
+    secretAccessKey: "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"
+})
+var s3 = new AWS.S3();
+
+// var params = {
+//     Bucket: 'test-s3-sdk1'
+// }
+
+
+
+//Delate Bucket
+// s3.deleteBucket(params,(err,data)=>{
+//     if(err){
+//         console.log(err, err.stack)
+//     }
+//     else
+//     {
+//         console.log(data)
+//     }
+// })
+
+// Bucket creation
+
+// var bucketName = 'test-s3-sdk4'+uuid.v4()
+// s3.createBucket({Bucket:bucketName},function(){
+//     var data={
+//         Bucket:bucketName,
+//         Key: "test.html" 
+
+//     };
+
+//     s3.putObject(data,function(err,data){
+//        if(err){
+//            console.log(err)
+//        } 
+//        else
+//        {
+//            console.log(data)
+//        }
+//     })
+// })
+
+
+//List all buckets
+
+s3.listBuckets((err,data)=>{
+    if(err){
+        console.log(err,err.stack)
+    }
+    else
+    {
+        console.log(data)
+    }
+})
+
